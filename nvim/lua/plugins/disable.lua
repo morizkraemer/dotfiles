240dbcdb59f1798d88189e6b0987053096286229
morizkraemer <morizkraemer@gmail.com>
2024-02-05 12:02:05 +0100

    update

--- nvim/lua/plugins/disable.lua
+++ nvim/lua/plugins/disable.lua
@@ -1,6 +1,6 @@
 return {
-  { "folke/flash.nvim", enabled = false },
-  -- { "stevearc/aerial.nvim", enabled = false },
-  { "folke/neoconf.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
+    { "folke/flash.nvim", enabled = false },
+    -- { "stevearc/aerial.nvim", enabled = false },
+    { "folke/neoconf.nvim", enabled = false },
+    { "akinsho/bufferline.nvim", enabled = false },
 }
